56754ba4e2761d00210bf20164b4e50a35428fa1
pkirme <kirmepriti@gmail.com>
2023-12-10 19:21:15 +0530

    object singleton

---
+++ ObjectSingleton.js
@@ -0,0 +1,13 @@
+//object singleton :Declare object using Object function
+const obj =new Object({
+    name : "abc",
+    age : 30,
+    location : "Nagpur"
+}); // Singleton object
+console.log(obj);
+
+const objNonSingleton ={
+    name : "abc",
+    age : 30,
+    location : "Nagpur"
+} // Non-singleton object
